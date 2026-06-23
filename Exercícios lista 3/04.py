4a2645ac3a7f229f21dbee5285bac7da583c6325
julis <batata.com>
2026-06-23 17:22:32 -0300

    Exercícios lista 3

---
+++ Exercícios lista 3/04.py
@@ -0,0 +1,13 @@
+placas = []
+
+opcao = -1
+while opcao != 0:
+    print("1 - Cadastrar")
+    print("2 - Excluir")
+    print("3 - Listar")
+    print("0 - Sair")
+
+    opcao_escolhida = int(input("Digite a opção escolhida :"))
+
+    if opcao == "1":
+        print("")
