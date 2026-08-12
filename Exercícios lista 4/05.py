f78d4ee06b18f64ca0874fcd37f6247829fedbda
Gamahen011 <ricaohgp@gmail.com>
2026-08-12 11:54:32 -0300

    Exercícios lista4

--- Exercícios lista 4/05.py
+++ Exercícios lista 4/05.py
@@ -1,3 +1,53 @@
 #Cria uma lista vazia de amigos proximos
-amigos_proximos = [] 
-#
+amigos_proximos = []
+#colocar uma variável diferente de 0 para o programa aceitar 
+opcao = -1
+#enquanto for diferente de 0
+while opcao != 0 :
+#Lista as opcoes para o usuário
+    print("=== AMIGOS PROXIMOS ===")
+    print("1 - Cadastrar")
+    print("2 - Excluir")
+    print("3 - Listar")
+    print("0 - Sair")
+
+    opcao = int(input("Escolha uma opção: "))
+
+    if opcao == 1:
+        # verifica se o amigo ja esta cadastrado
+            amigo = input("Digite o nome do novo amigo: ")
+
+            if amigo in amigos_proximos:
+                print("Esse amigo ja esta na lista.")
+            else:
+                indice = amigos_proximos.index(amigo)
+                amigos_proximos[indice] = amigo
+                print("Ebaaaa, novo amigo aicionado!")
+
+    elif opcao == 2:
+        amigo = input("Digite o amigo que deseja excluir: ").upper()
+
+        if amigo in amigos_proximos:
+            indice = amigos_proximos.index(amigo)
+            amigos_proximos[indice] = None
+            print("Pessoa excluida com sucesso...")
+        else:
+            print("Falha: Este usuario não foi encontrado e voce deveria saber.")
+
+    elif opcao == 3:
+        print("AMIGOS PROXIMOS:")
+        existe = False
+
+        for amigo in amigos_proximos:
+            if amigo is not None:
+                print(amigo)
+                existe = True
+
+        if not existe:
+            print("Voce nao tem amigos.")
+
+    elif opcao == "0":
+        print("Progama encerrado.")
+        break
+
+    
