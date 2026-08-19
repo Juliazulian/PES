76c85c90ebeca6b2246e2bdf33e7b955f5af4335
julis <batata.com>
2026-08-19 09:37:08 -0300

    Exercícios lista 4.5

---
+++ Exercícios lista 4.5/01.py
@@ -0,0 +1,32 @@
+prof = [{
+    "cod": "001",
+    "nome": "Prof Thiago Paes"
+}, {
+    "cod": "002",
+    "nome": "Prof Schalata"
+}, {
+    "cod": "003",
+    "nome": "Prof Ignácio"
+}, {
+    "cod": "004",
+    "nome": "Prof Ryan"
+}, {
+    "cod": "005",
+    "nome": "Prof André"
+}, {
+    "cod": "006",
+    "nome": "Profª Fabiana"
+}, {
+    "cod": "007",
+    "nome": "Prof Alberto"
+}, {
+    "cod": "008",
+    "nome": "Prof Juliano"
+}, {
+    "cod": "009",
+    "nome": "Prof Thiago Waltrik"
+}, {
+    "cod": "010",
+    "nome": "Prof João Eduardo"
+}]
+
