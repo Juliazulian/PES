0b9881742b3a8c33b2308e33763bcb0da61b8499
julis <batata.com>
2026-08-19 09:36:53 -0300

    Exercícios lista 4

--- Exercícios lista 4/03.py
+++ Exercícios lista 4/03.py
@@ -4,12 +4,12 @@ notas = []
 y = int(input("Quantas notas devem ser digitadas? :"))
 #repete a nota (y) vezes (quantas o usuário quiser)
 for i in range(y):
-    #le a nota e deixa ela ter decimais
-    nota = float(input("Digite as notas :"))
-    #adiciona a nota a lista
-    notas.append(nota)
-    #le a lista toda e mostra todas as notas
-    for nota in notas:
+   #le a nota e deixa ela ter decimais
+   nota = float(input("Digite as notas :"))
+   #adiciona a nota a lista
+   notas.append(nota)
+   #le a lista toda e mostra todas as notas
+   for nota in notas:
       print(nota)
 #Com while
 i = 0 #começa na primeira posição da lista
