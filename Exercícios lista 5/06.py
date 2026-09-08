2d2f94576b9f23794d057d7655eb78b335c42bc6
Gamahen011 <ricaohgp@gmail.com>
2026-09-08 08:50:38 -0300

    Exercícios lista  5

---
+++ Exercícios lista 5/06.py
@@ -0,0 +1,6 @@
+tempo_minutos = int(input("Digite o tempo em minutos: \n-"))
+tempo_horas = int(input("Digite o tempo em horas: \n-"))
+def tempo_total():
+    if tempo_horas > 60:00
+    minutos_transformados = tempo_horas * 60
+    print (minutos_transformados) 
