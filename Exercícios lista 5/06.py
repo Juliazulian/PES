601e9af833ea5526a8e809c5d25c1e7562d29336
luisablankenburg08 <luisanarvazblankenburg@gmail.com>
2026-09-22 08:43:51 -0300

    Exercícios lista  5

--- Exercícios lista 5/06.py
+++ Exercícios lista 5/06.py
@@ -1,6 +1,9 @@
+def tempo_total(tempo_horas, tempo_minutos):
+    minutos_transformados = tempo_horas * 60
+    total = tempo_minutos + minutos_transformados
+    return total
+
 tempo_minutos = int(input("Digite o tempo em minutos: \n-"))
 tempo_horas = int(input("Digite o tempo em horas: \n-"))
-def tempo_total():
-    if tempo_horas > 60:00
-    minutos_transformados = tempo_horas * 60
-    print (minutos_transformados) 
+
+print(tempo_total)
